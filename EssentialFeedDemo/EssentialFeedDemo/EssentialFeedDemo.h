20dceb1e04add4bbe0b4b147c6e9b856e6b7581e
dhanrajbhandari <66944662+dhanrajbhandari@users.noreply.github.com>
2021-07-09 14:05:21 +0530

    Add files via upload
    Xcode file added

---
+++ EssentialFeedDemo/EssentialFeedDemo/EssentialFeedDemo.h
@@ -0,0 +1,20 @@
+//
+//  EssentialFeedDemo.h
+//  EssentialFeedDemo
+//
+//  Created by Dhanraj Bhandari on 09/07/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeedDemo.
+FOUNDATION_EXPORT double EssentialFeedDemoVersionNumber;
+
+//! Project version string for EssentialFeedDemo.
+FOUNDATION_EXPORT const unsigned char EssentialFeedDemoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeedDemo/PublicHeader.h>
+
+
+
+
