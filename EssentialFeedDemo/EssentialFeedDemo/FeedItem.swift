2e92b8baf5c4bd3eae07e144b7c8fdab9605312b
Dhanraj <dhanraj@kloojj.com>
2021-07-12 09:49:01 +0530

    Feed Item Model

---
+++ EssentialFeedDemo/EssentialFeedDemo/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeedDemo
+//
+//  Created by Dhanraj Bhandari on 12/07/21.
+//
+
+import Foundation
+
+struct FeetItem{
+    let id : UUID
+    let description: String?
+    let location: String?
+    let url: URL
+}
